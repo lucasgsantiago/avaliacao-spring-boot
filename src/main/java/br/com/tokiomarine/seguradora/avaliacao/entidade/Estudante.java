8c5c4c32e66180e530ea937b42088d0c4ebf37d8
LucasGS <lucasgomessantiago@gmail.com>
2020-05-10 14:12:50 -0300

    Item #2 - Implementação da entidade Estudante.

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -1,5 +1,62 @@
 package br.com.tokiomarine.seguradora.avaliacao.entidade;
 
-public class Estudante {
-	// TODO Implementar a entidade Estudante conforme solicitado
+import br.com.tokiomarine.seguradora.avaliacao.helpers.BusinessException;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Estudante implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Nome é obrigatório")
+    @Column()
+    private String nome;
+
+    @NotNull(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    private String telefone;
+
+    public Estudante(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Estudante(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public void alterar(String nome, String email, String telefone) throws BusinessException {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.validar();
+    }
+
+    public void validar() throws BusinessException {
+        Set<ConstraintViolation<Estudante>> constraintViolations = Validation.buildDefaultValidatorFactory().getValidator().validate(this);
+        if(constraintViolations != null && !constraintViolations.isEmpty()){
+            for (ConstraintViolation error: constraintViolations) {
+                throw new BusinessException(error.getMessage());
+            }
+        }
+
+    }
+
 }
